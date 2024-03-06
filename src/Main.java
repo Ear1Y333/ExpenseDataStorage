f23e08a77176ffc74ddb0a97dff9237d370cf605
Ear1Y333 <135262702+Ear1Y333@users.noreply.github.com>
2024-03-06 16:19:31 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ExecuteRequest executeRequest = new ExecuteRequest(new SQLConnection().getConnection());
-        executeRequest.getValues();
-        executeRequest.insertValues(1,4,23,4, "1978-2-12");
-    }
-}
-
