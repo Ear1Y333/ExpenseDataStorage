ee8962b01bbe8cc654853107071ba0a4aeeda8ba
Ear1Y333 <135262702+Ear1Y333@users.noreply.github.com>
2024-03-17 16:55:30 +0300

    Delete Backend directory

--- Backend/src/main/java/mySpring/ExpenseDataSrorage/Model/GoodCreateDto.java
+++
@@ -1,9 +0,0 @@
-package mySpring.ExpenseDataSrorage.Model;
-
-import lombok.Data;
-
-@Data
-public class GoodCreateDto {
-    private String good_name;
-    private int type;
-}
--- Backend/src/main/java/mySpring/ExpenseDataSrorage/Model/GoodTypeCreateDto.java
+++
@@ -1,8 +0,0 @@
-package mySpring.ExpenseDataSrorage.Model;
-
-import lombok.Data;
-
-@Data
-public class GoodTypeCreateDto {
-    private String good_type_name;
-}
